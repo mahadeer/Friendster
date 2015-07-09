295c801b776615d45f6a73a470798c507d82765e
Mahadeer Mohamed <mahadeer2010@gmail.com>
2015-07-10 00:53:40 +0530

    Parsed Data from JSON and rendered on the Table Cell

--- web/Helpers/JSONHelper.dart
+++ web/Helpers/JSONHelper.dart
@@ -1,6 +1,10 @@
 library JSONHelper;
 
+import 'dart:html';
+import 'dart:async';
+
 class JSONHelper {
-  JSONHelper() {
+  static Future<String> FetchData() {
+    return HttpRequest.getString('http://localhost:63342/Portfolio/web/data/data.json');
   }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:convert';
 import 'Helpers/JSONHelper.dart';
 import 'Helpers/FormParser.dart';
 import 'model/Person.dart';
@@ -6,11 +7,8 @@ import 'model/Person.dart';
 void main() {
 
   /* JSON initializers from local db*/
-  var db = new JSONHelper();
-
-  var submit = querySelector('#btn_AddUser') as ButtonElement;
-  submit.onClick.listen(fn_AddUser);
-
+  JSONHelper.FetchData().then(fn_LoadData);
+  fn_BindEvents();
 }
 
 void fn_AddUser(e) {
@@ -19,3 +17,29 @@ void fn_AddUser(e) {
   var person = new Person(data);
 }
 
+void fn_BindEvents() {
+  var submit = querySelector('#btn_AddUser') as ButtonElement;
+  submit.onClick.listen(fn_AddUser);
+}
+
+void fn_LoadData (String res) {
+  Map data =JSON.decode(res);
+  fn_GetFriends(data);
+}
+
+void fn_GetFriends (Map Response) {
+  List<Map> Friends = new List<Map>();
+  Friends = Response["Friends"];
+  TableElement table = querySelector('#tbl_friendsList') as TableElement;
+  var tBody = table.createTBody();
+  for (Map _friend in Friends) {
+    fn_CreateCellItem(_friend, tBody);
+  }
+}
+
+void fn_CreateCellItem (Map _friend, TableSectionElement tBody) {
+  TableRowElement newLine = tBody.insertRow(-1);
+  newLine.insertCell(0).text = '${_friend["FirstName"]}, ${_friend["FamilyName"]}';
+  newLine.insertCell(1).text = _friend["Sex"];
+  newLine.insertCell(2).text = _friend["Country"];
+}
